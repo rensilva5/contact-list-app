e82f6421bf2e6658edf2a955f2bc48a399ea6c0b
rensilva5 <renzoasilva@gmail.com>
2022-11-16 09:39:51 -0500

    using props

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ import ContactCard from './ContactCard';
 function App() {
   return (
     <div>
-    <ContactCard />
-    <ContactCard />
-    <ContactCard />
+    <ContactCard
+      avatarUrl="https://via.placeholder.com/150"
+      name= "Jason Momoa"
+      email= "jasonmomoa@fakeemail.com"
+      age= {28}/>
+    <ContactCard
+      avatarUrl="https://via.placeholder.com/150"
+      name= "Paul Smith"
+      email= "paulsmith@fakeemail.com"
+      age= {25}/>
+    <ContactCard
+      avatarUrl="https://via.placeholder.com/150"
+      name= "James Arthur"
+      email= "jamesarthur@fakeemail.com"
+      age= {41}/>
     </div>
   );
 }
--- src/ContactCard.jsx
+++ src/ContactCard.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
 
-export default function ContactCard() {
+export default function ContactCard(props) {
     const [showAge, setShowAge] = useState(true)
   return (
       <div className="contact-card">
-      <img src="https://via.placeholder.com/150" alt="profile" />      
+      <img src={props.avatarUrl} alt="profile" />      
       <div className="user-details">
-        <p>Name: Jason Momoa</p>
-        <p>Email: jason.momoa@fakeemail.com</p>
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email}</p>
         <button onClick={() => setShowAge(!showAge)}></button>
-        {showAge && <p>Age: 28</p>}
+        {/* {showAge ? <p>Age: 28</p> : null}  // optional */}
+        {showAge && <p>Age: {props.age}</p>}
       </div>
     </div>
   )
