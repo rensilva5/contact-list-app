c3f41e7ca83e61165cea959d814d705466399bcd
rensilva5 <renzoasilva@gmail.com>
2022-11-15 23:11:44 -0500

    adding components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import './App.css';
+import ContactCard from './ContactCard';
 
 function App() {
-
-  const title = "hello World"
   return (
-    <div className="App">
-      
-      
+    <div>
+    <ContactCard />
+    <ContactCard />
+    <ContactCard />
     </div>
   );
 }
---
+++ src/ContactCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ContactCard() {
+  return (
+      <div className="contact-card">
+      <img src="https://via.placeholder.com/150" alt="profile" />      
+      <div className="user-details">
+        <p>Name: Jason Momoa</p>
+        <p>Email: jason.momoa@fakeemail.com</p>
+        <p>Age: 28</p>
+      </div>
+    </div>
+  )
+}
